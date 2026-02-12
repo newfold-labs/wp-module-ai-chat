ca1b2f4d0ce65bb46160ba387e39cd163256c001
arunshenoy99 <devarunshenoy99@gmail.com>
2026-02-13 00:51:19 +0530

    chore: remove unused config/constants.js (duplicate of constants/nfdAgents/input.js)

--- src/config/constants.js
+++
@@ -1,9 +0,0 @@
-/**
- * Input Component Configuration
- * Constants for chat input behavior (used by ChatInput).
- */
-export const INPUT = {
-	MAX_HEIGHT: 200, // Textarea max height (px)
-	FOCUS_DELAY: 100, // Delay before focusing input (ms)
-	STOP_DEBOUNCE: 500, // Debounce for stop button (ms)
-};
