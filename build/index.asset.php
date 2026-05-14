7eceecf6d817534d1d0b933a77b016c78f7935a1
zlateska <hzlateska@gmail.com>
2026-05-14 11:49:28 -0400

    Rebuild bundle

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '991193301f910f544f91');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '65d84f0a9ceeec4b6641');
